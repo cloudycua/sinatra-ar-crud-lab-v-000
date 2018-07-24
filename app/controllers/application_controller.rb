aa1ac8bb17f76e80226559f7a5dffe35d60275de
Christine Cua <cloudycua@gmail.com>
2018-07-24 22:56:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,10 @@ class ApplicationController < Sinatra::Base
   get '/posts/new' do
     erb :new
   end
+
+# creates a post from form submittion and adds to table
+  get '/posts' do
+    @post = Post.create(params)
+    redirect to '/posts'
+  end
 end
