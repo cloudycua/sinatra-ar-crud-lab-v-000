bb1339919d658078761a9ce941de0de8ddb876bd
Christine Cua <cloudycua@gmail.com>
2018-07-24 22:57:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,12 @@ class ApplicationController < Sinatra::Base
     @post = Post.create(params)
     redirect to '/posts'
   end
+
+# create @posts variable containing all the osts to pass to view
+# loads index page which iterates through @posts to display all the blog posts
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+  
 end
